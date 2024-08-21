3039d9835b1f5f3a92479eeea68a11229cc45858
Abhishek-1698 <abhishektodupunuri3@gmail.com>
2024-08-21 09:18:24 +0530

    Auto Update

--- 015-loops.sh
+++ 015-loops.sh
@@ -3,8 +3,8 @@
 # Loops : When you want to run something for some number of times continuously, then we use LOOPS 
 
 # There are 2 variaties of loops : 
-#     1) For Loop        ( When you known how many times to be executed, we use for loop )
-#     2) While Loop      ( when you don't know how many time to be executed, we use this and while only runs if the condition is trye )
+#     1) For Loop        ( When you know how many times to be executed, we use for loop )
+#     2) While Loop      ( when you don't know how many time to be executed, we use this and whileloop only runs if the condition is true )
 
 for var in a b c d ; do 
     echo "Value is $var" 
